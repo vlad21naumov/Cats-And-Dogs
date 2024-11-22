515e500c3da27400866681f52cf89432ab58d49c
naumov-vladis <naumov-vladis@yandex.team>
2024-11-22 20:31:42 +0300

    test commit

---
+++ cats-and-dogs/train.py
@@ -0,0 +1,71 @@
+import os
+
+import pytorch_lightning as pl
+import torchvision
+import torchvision.transforms as transforms
+from constants import (
+    BATCH_SIZE,
+    DATA_PATH,
+    IMAGE_MEAN,
+    IMAGE_STD,
+    LR,
+    MODELS_PATH,
+    N_CHANNELS,
+    N_EPOCHS,
+    NUM_CLASSES,
+    NUM_WORKERS,
+    SIZE_H,
+    SIZE_W,
+)
+from model import SimpleClassifier
+from torch.utils.data import DataLoader
+from trainer import ImageClassifier
+
+
+def main():
+    transformer = transforms.Compose(
+        [
+            transforms.Resize((SIZE_H, SIZE_W)),
+            transforms.ToTensor(),
+            transforms.Normalize(IMAGE_MEAN, IMAGE_STD),
+        ]
+    )
+
+    train_dataset = torchvision.datasets.ImageFolder(
+        os.path.join(DATA_PATH, "train_11k"), transform=transformer
+    )
+
+    val_dataset = torchvision.datasets.ImageFolder(
+        os.path.join(DATA_PATH, "val"), transform=transformer
+    )
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+    )
+    val_loader = DataLoader(
+        val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS
+    )
+
+    model = SimpleClassifier(N_CHANNELS, SIZE_H, SIZE_W, NUM_CLASSES)
+    module = ImageClassifier(model, lr=LR)
+
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        monitor="val_loss",
+        dirpath=MODELS_PATH,
+        filename="model_{val_loss:.2f}",
+        save_top_k=1,
+        mode="min",
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=N_EPOCHS,
+        accelerator="auto",
+        devices="auto",
+        callbacks=[checkpoint_callback],
+    )
+
+    trainer.fit(module, train_loader, val_loader)
+
+
+if __name__ == "__main__":
+    main()
