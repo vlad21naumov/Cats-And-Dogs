f4a25e95e2f37e74acd9c4d6b6d8c4cef8dbbdf2
naumov-vladis <naumov-vladis@yandex.team>
2024-11-22 20:48:20 +0300

    add files

---
+++ scripts/download_data.py
@@ -0,0 +1,28 @@
+import os
+
+import fire
+
+
+def download_and_extract(url, target_dir):
+    """
+    Downloads a ZIP file from a URL using wget, extracts it using unzip,
+    and deletes the ZIP file after extraction.
+    """
+    os.makedirs(target_dir, exist_ok=True)
+
+    zip_path = os.path.join(target_dir, "data.zip")
+    wget_command = f"wget -nc -O {zip_path} '{url}'"
+    print(f"Executing: {wget_command}")
+    os.system(wget_command)
+
+    unzip_command = f"unzip -n -qq {zip_path} -d {target_dir}"
+    print(f"Executing: {unzip_command}")
+    os.system(unzip_command)
+
+    print(f"Deleting {zip_path}...")
+    os.remove(zip_path)
+    print(f"Deleted {zip_path}.")
+
+
+if __name__ == "__main__":
+    fire.Fire(download_and_extract)
