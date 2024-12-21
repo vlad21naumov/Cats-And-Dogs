b4bd3641ceddb315d639b5da5f5e8d9c07cf7183
naumov-vladis <naumov-vladis@yandex.team>
2024-12-21 11:58:45 +0300

    add loggers and hydra

--- cats_and_dogs/model_selector.py
+++ cats_and_dogs/model_selector.py
@@ -1,15 +1,22 @@
+from typing import Any, Dict
+
 import torch
 
-from constants import N_CHANNELS, NUM_CLASSES, SIZE_H, SIZE_W
 from conv_model import ConvClassifier
 from model import SimpleClassifier
 
 
-def get_model(label: str) -> torch.nn.Module:
+def get_model(model_conf: Dict[str, Any]) -> torch.nn.Module:
     """Select model to run experiments"""
+    label = model_conf["label"]
     if label == "linear":
-        return SimpleClassifier(N_CHANNELS, SIZE_H, SIZE_W, NUM_CLASSES)
+        return SimpleClassifier(
+            model_conf["num_channels"],
+            model_conf["image_height"],
+            model_conf["image_width"],
+            model_conf["num_classes"],
+        )
     if label == "conv":
-        return ConvClassifier(NUM_CLASSES)
+        return ConvClassifier(model_conf["num_classes"])
     else:
         raise ValueError(f"There is no such model with label {label}")
--- cats_and_dogs/train.py
+++ cats_and_dogs/train.py
@@ -1,66 +1,72 @@
 import os
 
-import fire
+# import fire
+import hydra
 import pytorch_lightning as pl
 import torchvision
 import torchvision.transforms as transforms
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
-from constants import (
-    BATCH_SIZE,
-    DATA_PATH,
-    IMAGE_MEAN,
-    IMAGE_STD,
-    LR,
-    MODELS_PATH,
-    N_EPOCHS,
-    NUM_WORKERS,
-    SIZE_H,
-    SIZE_W,
-)
+from logger_selector import get_logger
 from model_selector import get_model
 from trainer import ImageClassifier
 
 
-def main(model_label: str) -> None:
+@hydra.main(version_base=None, config_path="../conf", config_name="config")
+def main(config: DictConfig):
     transformer = transforms.Compose(
         [
-            transforms.Resize((SIZE_H, SIZE_W)),
+            transforms.Resize(
+                (config["model"]["image_height"], config["model"]["image_width"])
+            ),
             transforms.ToTensor(),
-            transforms.Normalize(IMAGE_MEAN, IMAGE_STD),
+            transforms.Normalize(
+                config["model"]["image_mean"], config["model"]["image_std"]
+            ),
         ]
     )
 
     train_dataset = torchvision.datasets.ImageFolder(
-        os.path.join(DATA_PATH, "train_11k"), transform=transformer
+        os.path.join(config["data_loading"]["train_data_path"]), transform=transformer
     )
 
     val_dataset = torchvision.datasets.ImageFolder(
-        os.path.join(DATA_PATH, "val"), transform=transformer
+        os.path.join(config["data_loading"]["val_data_path"]), transform=transformer
     )
 
     train_loader = DataLoader(
-        train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+        train_dataset,
+        batch_size=config["training"]["batch_size"],
+        shuffle=True,
+        num_workers=config["training"]["num_workers"],
     )
     val_loader = DataLoader(
-        val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS
+        val_dataset,
+        batch_size=config["training"]["batch_size"],
+        shuffle=False,
+        num_workers=config["training"]["num_workers"],
     )
 
-    model = get_model(model_label)
-    module = ImageClassifier(model, lr=LR)
+    model = get_model(config["model"])
+    module = ImageClassifier(model, lr=config["training"]["lr"])
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         monitor="val_loss",
-        dirpath=MODELS_PATH,
+        dirpath=config["model"]["model_local_path"],
         filename="model_{val_loss:.2f}",
         save_top_k=1,
         mode="min",
     )
 
+    logger = get_logger(config["logging"])
+
     trainer = pl.Trainer(
-        max_epochs=N_EPOCHS,
+        max_epochs=config["training"]["num_epochs"],
+        log_every_n_steps=1,  # to resolve warnings
         accelerator="auto",
         devices="auto",
+        logger=logger,
         callbacks=[checkpoint_callback],
     )
 
@@ -68,4 +74,4 @@ def main(model_label: str) -> None:
 
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    main()
