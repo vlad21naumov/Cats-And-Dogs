862416cfd68ca1419d442351250751e3e15cf3d2
naumov-vladis <naumov-vladis@yandex.team>
2025-02-28 17:53:21 +0300

    add val acc

--- cats_and_dogs/train.py
+++ cats_and_dogs/train.py
@@ -78,6 +78,8 @@ def train_model(model, train_loader, val_loader, loss_fn, opt, device, n_epochs:
         f"../models/simple_model_{np.round(top_val_accuracy, 2)}.pt",
     )
     print(f"Final validation loss: {val_loss[-1]:.1f}")
+    print(f"Best validation accuracy: {100 * top_val_accuracy:.1f}")
+
     return train_loss, val_loss, val_accuracy, best_model
 
 
