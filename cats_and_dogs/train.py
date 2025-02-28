415c0e3f5423c27b72e4cfa526f0ac172871b691
naumov-vladis <naumov-vladis@yandex.team>
2025-02-28 22:06:33 +0300

    add log best val acc

--- cats_and_dogs/train.py
+++ cats_and_dogs/train.py
@@ -76,6 +76,7 @@ def train_model(model, train_loader, val_loader, loss_fn, opt, device, n_epochs:
         best_model.state_dict(),
         f"../models/simple_model_{np.round(top_val_accuracy, 2)}.pt",
     )
+    print(f"Best validation accuracy: {100 * top_val_accuracy:.1f}")
     return train_loss, val_loss, val_accuracy, best_model
 
 
